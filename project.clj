916b415c97c19ac05a647f69df42234ae0c7e5c9
Damien Stanton <damien.stanton@gmail.com>
2023-07-29 07:33:39 -0400

    deps.edn, merge upstream PR #4

--- project.clj
+++
@@ -1,6 +0,0 @@
-(defproject special "0.1.3-Beta1"
-  :description "Special (Conditions). A minimalistic condition library that is not complected with exception handling. "
-  :url "https://github.com/clojureman/special"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
